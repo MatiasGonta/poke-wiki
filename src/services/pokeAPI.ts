cfff02b9b7b59824178158dca1f19ba33a6c8120
Matías Gonta <maret2023@gmail.com>
2024-02-21 11:48:42 -0300

    Delete dead code :coffin:

--- src/services/pokeAPI.ts
+++ src/services/pokeAPI.ts
@@ -1,7 +1,1 @@
-import axios, { AxiosInstance } from "axios";
-
-export const api: AxiosInstance = axios.create({
-  baseURL: 'https://pokeapi.co/api/v2',
-});
-
 export const BASE_URL = 'https://pokeapi.co/api/v2';
